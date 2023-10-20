967f2d5227b3ccf2f3067f91a200bbf28bfa11f7
Om Shende <110690841+om278@users.noreply.github.com>
2023-10-20 10:53:45 +0530

    Add files via upload

---
+++ shareposts/app/controllers/Pages.php
@@ -0,0 +1,46 @@
+<?php
+  class Pages extends Controller {
+    public function __construct(){
+     
+    }
+    
+    public function index(){
+      if(isLoggedIn()){
+        redirect('posts');
+      }
+      $data = [
+        'title' => 'SharePosts',
+        'description' => 'Simple social network built on the Emmizy MVC framework',
+        'info' => 'You can contact me with the following details below if you like my program and willing to offer me a contract and work on your project',
+        'name' => 'Om Shende',
+        'location' => 'Nagpur, Maharastra State',
+        'contact' => '+91 9359086635',
+        'mail' => 'om@gmail.com'
+      ];
+     
+      $this->view('pages/index', $data);
+    }
+
+    public function about(){
+      $data = [
+        'title' => 'About Us',
+        'description' => 'App to share posts with other users'
+      ];
+
+      $this->view('pages/about', $data);
+    }
+
+    public function contact(){
+      $data = [
+          'title' => 'Contact Us',
+          'description' => 'You can contact us through this medium',
+          'info' => 'You can contact me with the following details below if you like my program and willing to offer me a contract and work on your project',
+          'name' => 'Om Shende',
+          'location' => 'Nagpur, Maharastra State',
+          'contact' => '+9 9359086635',
+          'mail' => 'om@gmail.com'
+      ];
+
+      $this->view('pages/contact', $data);
+    }
+  }
